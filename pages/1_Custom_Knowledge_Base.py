17eb2a2a1c3c19105779aea1106def0108454ddf
orsmember <81900977+orsmember@users.noreply.github.com>
2024-05-08 08:28:40 +0200

    Update 1_Custom_Knowledge_Base.py

--- pages/1_Custom_Knowledge_Base.py
+++ pages/1_Custom_Knowledge_Base.py
@@ -7,7 +7,9 @@ Created on Fri Jul 28 09:13:38 2023
 
 import streamlit as st
 from streamlit_pills import pills
-from llama_index import SimpleDirectoryReader, GPTVectorStoreIndex, ServiceContext
+#from llama_index import SimpleDirectoryReader, GPTVectorStoreIndex, ServiceContext
+from llama_index.core import SimpleDirectoryReader, GPTVectorStoreIndex, ServiceContext
+
 from pathlib import Path
 import os
 import openai
